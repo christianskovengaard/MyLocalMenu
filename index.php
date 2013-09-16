80a71c15894001bd6135c556130835a008000498
bejtrup <bejtrup@gmail.com>
2013-09-16 13:28:57 +0200

    opsaetning af index.php

--- index.php
+++ index.php
@@ -2,11 +2,61 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <meta name="viewport" content="width=device-width, maximum-scale=1.0, minimun-scale=1.0, initial-scale=1.0" />
+        <title>My Local Menu</title>
+        <link rel="stylesheet" type="text/css" href="css/general_index.css" />
+        <link rel="stylesheet" type="text/css" media="only screen and (min-width:50px) and (max-width: 500px)" href="css/general_index_small.css" />
+        <link rel="stylesheet" type="text/css" media="only screen and (min-width:501px) and (max-width: 800px)" href="css/general_index_medium.css" />
+                
+        <!--[if lt IE 9]>
+            <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+        
     </head>
     <body>
         <?php
         // put your code here
         ?>
+        
+        <div class="header">
+            <div class="wrapper">
+                My Local Menu [logo]<br>
+                    <input type="text" placeholder="Email">
+                    <input type="Password" placeholder="Kodeord">
+                    <div onclick="" class="button01">Log ind</div>
+            </div>
+        </div> 
+        
+        <div class="info01">
+            <div class="wrapper">
+                info01 [billeder]<br>
+                <input type="text"><div onclick="" class="button01" placeholder="Find Menukord">SÃ¸g</div>
+            </div>
+        </div> 
+        
+        <div class="info02">
+            <div class="wrapper">
+                <h1>Opret et menukort</h1>
+                <h3>...det er helt gratis</h3>
+                <input type="text" placeholder="Indtast din email">
+                <input type="Password" placeholder="Indtast et kodeord">
+                <div onclick="" class="button01">Opret</div>
+            </div>
+        </div> 
+        
+        <div class="info03">
+            <div class="wrapper">
+                <h2>Du kan altid opdater dit menukort</h2>
+                <p>lsgk Ã¦naerjl Ã¦nbalkbkmer vker iogeja iopjarg weiop wpjag jklrg laweip seflEFK Lf fff lsgkÃ¦naer jlÃ¦nba lkbkmer vker iogeja iopjarg weiop wpjag jklrg laweip seflEFK Lf fff</p>
+                <h2>Virker pÃ¥</h2>
+            </div>
+        </div>
+        
+        <div class="footer">
+            <div class="wrapper">
+                Firma info kontakt ect
+            </div>
+        </div> 
+        
     </body>
 </html>
