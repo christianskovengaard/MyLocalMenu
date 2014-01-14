d8857bc9464014efa330ad2ca589eaf1a5779d35
Christian Skovengaard <christian@headnet.dk>
2014-01-14 15:02:13 +0100

    Changed name of files.

--- login.php
+++ login.php
@@ -6,10 +6,14 @@ if(isset($_POST['username']) && isset($_POST['password']))
     require 'Controllers/UserController.php';
     $oUserController = new UserController();
     $loggedIn = $oUserController->LogInUser($_POST['username'], $_POST['password']);
-    header('location: admin.php');
+   if($loggedIn == true){
+        header('location: admin.php');
+   }else{echo "Kunne ikke logge ind! Prøv igen";
+   echo "<a href='fakeLogin.php'>Login side</a>";
+   }
 }
 else {
-    echo "Forkert brugernavn eller kodeord. Prøv igen!";
+    echo "Indtast brugernavn og kodeord. Prøv igen!";
     echo "<a href='fakeLogin.php'>Login side</a>";
 }
 ?>
