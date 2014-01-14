80f7edf6c85037f3f078b0727c218b4d00812a22
Christian Skovengaard <christian@headnet.dk>
2014-01-14 15:03:00 +0100

    Added code to delete session cookie

--- logout.php
+++ logout.php
@@ -2,6 +2,8 @@
     session_start();
     $_SESSION = array();
     session_destroy();
+    setcookie("PHPSESSID", "", -3600, "/");
+    
     setcookie('sec_session_id', '', time()-3600,'/', '', 0, 0);
     
     header("location: fakeLogin.php");
