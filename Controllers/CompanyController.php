a42f2acc57f11028df4d1ed1bbfc83d83d7b1825
Christian Skovengaard <christian@headnet.dk>
2014-01-17 10:39:49 +0100

    Added Controllers for Company and Email

--- Controllers/CompanyController.php
+++ Controllers/CompanyController.php
@@ -2,11 +2,48 @@
 
 class CompanyController 
 {
+    private $conPDO;
+    private $oCompany;
+    
     public function __construct() {
-        
+       
+       if(!class_exists('Database') )
+       { 
+            require 'DatabaseController.php';
+            $oDatabaseController = new DatabaseController();
+            $this->conPDO = $oDatabaseController->ConnectToDatabase(); 
+       }
+       
        require_once(ROOT_DIRECTORY . '/Classes/CompanyClass.php');
+       $this->oCompany = new CompanyClass();
     }
     
+    public function AddCompany($sCompanyName, $sCompanyPhone, $sCompanyAddress, $sCompanyZipcode, $sCompanyCVR)
+    {
+        
+        $this->oCompany->SetCompany($sCompanyName, $sCompanyPhone, $sCompanyAddress, $sCompanyZipcode, $sCompanyCVR);
+        
+        //Insert into database          
+        $sQuery = $this->conPDO->prepare("INSERT INTO company (sCompanyName,sCompanyPhone,sCompanyAddress,sCompanyZipcode,sCompanyCVR) VALUES (:sCompanyName,:sCompanyPhone,:sCompanyAddress,:sCompanyZipcode,:sCompanyCVR)");
+
+        $oCompany = $this->oCompany->GetCompany();
+        
+        $sQuery->bindValue(':sCompanyName', $oCompany->sCompanyName);
+        $sQuery->bindValue(':sCompanyPhone', $oCompany->sCompanyPhone);
+        $sQuery->bindValue(':sCompanyAddress', $oCompany->sCompanyAddress);
+        $sQuery->bindValue(':sCompanyZipcode', $oCompany->sCompanyZipcode);
+        $sQuery->bindValue(':sCompanyCVR', $oCompany->sCompanyCVR);
+        $sQuery->execute();
+        
+        //Get the last inserted id
+        $iCompanyId = $this->conPDO->lastInsertId();
+        
+        return $iCompanyId;
+         
+    }
     
+    public function __destruct() {
+        ;
+    }
 }
 ?>
---
+++ Controllers/EmailController.php
@@ -0,0 +1,13 @@
+<?php
+
+class EmailController
+{
+    public function __construct() {
+        ;
+    }
+    
+    public function __destruct() {
+        ;
+    }
+}
+?>
