340dd9d4216c8f7761d2130ec22d976b610a6965
Christian Skovengaard <christian@headnet.dk>
2014-01-17 10:39:17 +0100

    Added classes for Company and restuarant

--- Classes/CompanyClass.php
+++ Classes/CompanyClass.php
@@ -1,7 +1,37 @@
 <?php
 
 class CompanyClass {
-    //put your code here
+    
+    private $sCompanyName;
+    private $sCompanyPhone;
+    private $sCompanyAddress;
+    private $sCompanyZipcode;
+    private $sCompanyCVR;
+    
+    public function __construct() {
+        ;
+    }
+    
+    public function SetCompany($sCompanyName,$sCompanyPhone,$sCompanyAddress,$sCompanyZipcode,$sCompanyCVR)
+    {
+        $this->sCompanyName = $sCompanyName;
+        $this->sCompanyPhone = $sCompanyPhone;
+        $this->sCompanyAddress = $sCompanyAddress;
+        $this->sCompanyZipcode = $sCompanyZipcode;
+        $this->sCompanyCVR = $sCompanyCVR;
+    }
+    
+    public function GetCompany()
+    {
+        $oCompany = new stdClass();
+        $oCompany->sCompanyName = $this->sCompanyName;
+        $oCompany->sCompanyPhone = $this->sCompanyPhone;
+        $oCompany->sCompanyAddress = $this->sCompanyAddress;
+        $oCompany->sCompanyZipcode = $this->sCompanyZipcode;
+        $oCompany->sCompanyCVR = $this->sCompanyCVR;
+        
+        return $oCompany;
+    }
 }
 
 ?>
--- Classes/RestuarentClass.php
+++ Classes/RestuarentClass.php
@@ -2,7 +2,35 @@
 
 
 class RestuarentClass {
-    //put your code here
+    
+    private $sRestuarentInfoName;
+    private $sRestuarentInfoPhone;
+    private $sRestuarentInfoAddress;
+    private $iFK_iCompanyInfoId;
+    
+    public function __construct() {
+        ;
+    }
+    
+    public function SetRestaurent($sRestuarentInfoName,$sRestuarentInfoPhone,$sRestuarentInfoAddress,$iFK_iCompanyInfoId)
+    {
+        $this->sRestuarentInfoName = $sRestuarentInfoName;
+        $this->sRestuarentInfoPhone = $sRestuarentInfoPhone;
+        $this->sRestuarentInfoAddress = $sRestuarentInfoAddress;
+        $this->iFK_iCompanyInfoId = $iFK_iCompanyInfoId;
+
+    }
+    
+    public function GetRestuarent()
+    {
+        $oRestuarent = new stdClass();
+        $oRestuarent->sRestuarentInfoName = $this->sRestuarentInfoName;
+        $oRestuarent->sRestuarentInfoPhone = $this->sRestuarentInfoPhone;
+        $oRestuarent->sRestuarentInfoAddress = $this->sRestuarentInfoAddress;
+        $oRestuarent->iFK_iCompanyInfoId = $this->iFK_iCompanyInfoId;
+        
+        return $oRestuarent;
+    }
 }
 
 ?>
