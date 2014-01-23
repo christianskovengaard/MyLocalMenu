580357d0d52eea4a28284452bfb4e9612ca9709e
Christian Skovengaard <christian@headnet.dk>
2014-01-23 10:19:04 +0100

    Added comment to user.php

--- user.php
+++ user.php
@@ -1,7 +1,6 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+echo "Page for editing user profile<br>";
+echo "<a href='index.php'>Til Index</a><br>";
+echo "<a href='admin.php'>Til Admin</a><br>";
 ?>
