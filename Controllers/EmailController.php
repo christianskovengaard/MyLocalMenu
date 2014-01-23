3f22ddfc25491703527c91d47867424387498b14
Christian Skovengaard <christian@headnet.dk>
2014-01-23 10:15:12 +0100

    Sending real email from EmailController

--- Controllers/EmailController.php
+++ Controllers/EmailController.php
@@ -6,7 +6,21 @@ class EmailController
         ;
     }
     
-    public function __destruct() {
+    public function SendEmail ($sTo,$sFrom,$sSubject,$sMessage)
+    {
+        $sReturnPath = 'info@mylocalcafe.dk';
+        $sReplayTo = 'info@mylocalcafe.dk';
+
+        $headers = 'MIME-Version: 1.0' . "\r\n";
+        $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+        $headers .= 'From: MyLocalCafe <'.$sFrom. ">\r\n";
+        $headers .= 'Return-Path: MyLocalCafe<'.$sReturnPath. ">\r\n";
+        $headers .= 'Reply-To: MyLocalCafe <'.$sReplayTo."> \r\n";
+       
+        mail($sTo,$sSubject,$sMessage, $headers," -f ".$sReturnPath);
+    }
+
+        public function __destruct() {
         ;
     }
 }
