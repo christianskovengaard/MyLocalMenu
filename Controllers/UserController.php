8153e18e3611f811360ead2cd6ff5fcd6f0ae299
Christian Skovengaard <christian@headnet.dk>
2014-01-30 14:05:10 +0100

    Added edit user and company info

--- Controllers/UserController.php
+++ Controllers/UserController.php
@@ -156,6 +156,10 @@ class UserController
     public function LogInUser($sUsername,$sUserPassword)
     {
         
+        $aLogin = array(
+            'result' => false
+        );
+        
         $sQuery = $this->conPDO->prepare("SELECT sUserPassword,iUserIdHashed,iUserId,sUsername FROM users WHERE sUsername = ? LIMIT 1");
 	$sQuery->bindValue(1, $sUsername);
         
@@ -178,13 +182,17 @@ class UserController
            // We check if the account is locked from too many login attempts
            if($this->oSecurity->checkbrute($user_id,$this->conPDO) == true)
            { 
+               
+                //Create user token for the user to reset there password
+                
                 // Account is locked for 2 hours
-                $sMessage = 'Din konto er blevet spærret i 2 timer. Klik her for at genåbne din konto';
+                $sMessage = 'Din konto er blevet spærret i 2 timer. Klik <a href="localhost/MyLocalMenu/user.php?sUserToken=123">her</a> for at genåbne din konto';
                 $sTo = 'christianskovengaard@gmail.com';
                 $sFrom = 'info@mylocalcafe.dk';
                 $sSubject = 'Konto spærret';
                 $this->oEmail->SendEmail($sTo, $sFrom, $sSubject, $sMessage);
-                return false;
+                $aLogin['result'] = 'Account locked';
+                return $aLogin;
            }
            else
            {
@@ -203,7 +211,8 @@ class UserController
                     $_SESSION['username'] = $username;
                     $_SESSION['login_string'] = hash('sha512', $password.$ip_address.$user_browser);
                     // Login successful.
-                    return true;
+                    $aLogin['result'] = 'true';
+                    return $aLogin;
                 }
                 else
                 {
@@ -214,8 +223,8 @@ class UserController
                     $sQuery->bindValue(':user_id', $user_id);
                     $sQuery->bindValue(':now', $now);
                     $sQuery->execute();
-                    //echo 'Login attempt';
-                    return false;	
+                    $aLogin['result'] = 'false';
+                    return $aLogin;	
                 }  
            }
         }
@@ -223,7 +232,8 @@ class UserController
         {
             // No user exists. 
             //echo 'No user';
-            return false;
+            $aLogin['result'] = 'false';
+            return $aLogin;
         }
         
      }
@@ -486,7 +496,218 @@ zRT9yVmqGJTgjz0E+cV8/0ODbzajfq9JLIj/aICn+BXft7sLt1fJz9fwAwU2
      
      public function ResetPassword()
      {
-         //TODO: Create new password for user
+         $aUser = array(
+                'sFunction' => 'ResetPassword',
+                'result' => false
+            ); 
+         
+        if(isset($_GET['sJSON']))
+        {
+            $aJSONInfo = json_decode($_GET['sJSON']);
+            //var_dump($aJSONInfo);
+                    
+            $password = $aJSONInfo->sPassword;       
+            $encrypted = base64_decode($password);
+
+/* DO NOT INDENT THIS CODE */
+        $privkey = '-----BEGIN RSA PRIVATE KEY-----
+MIIJKQIBAAKCAgEA4jCNEY3ZyZbhNksbKG+l+LX9fIEiLkrRy9roGTKJnr2TEuZ2
+8qvwRKeFbAs0wpMUS5/8hF+Fte2Qywqq9ARGRNzTcDxjz72VRwTf1tSTKAJUsHYL
+KbBWPsvZ8FWk9EzJqltdj1mYVKMWcm7Ham5gwydozgnI3VbX8HMY8EglycKIc43g
+C+liSUmloWHGCnfKrfSEQ2cuIjnupvodvFw65dAanLu+FRuL6hnvt7huxXz5+wbP
+I5/aFGWUIHUbHoDFOag8BhVaDjXCrjWt3ry3oFkheO87swYfSmQg4tHM/2keCrsd
+HAk2z/eCuYcbksnmNgTqWgcSHNGM+nq9ngz/xXeb1UT+KxBy7K86oCD0a67eDzGv
+u3XxxW5N3+vvKJnfL6xT0EWTYw9Lczqhl9lpUdCgrcYe45pRHCqiPGtlYIBCT5lq
+VZi9zncWmglzl2Fc4fhjwKiK1DH7MSRBO8utlgawBFkCprdsmapioTRLOHFRAylS
+GUiyqYg0NqMuQc5fMRiVPw8Lq3WeAWMAl8paksAQHYAoFoX1L+4YkajSVvD5+jQI
+t3JFUKHngaGoIWnQXPQupmJpdOGMCCu7giiy0GeCYrSVT8BCXMb4UwIr/nAziIOM
+iK87WAwJKysRoZH7daK26qoqpylJ00MMwFMPvtrpazOcbKmvyjE+Gg/ckzMCAwEA
+AQKCAgB3TVJqyt3vZSR+pZi6eEEbcKo17EqiDhagJmM7Pxu1XZpgYqykjKnbHFzU
+QwjeBAO1a7od++AjuB0h6wuGT2bc1Xi0fzXKEd3Vqq2Bu3eup6QRuwFiSL8EujLG
+f/XUYVgRAcXUYVZmderWCrYl3fgtlvDBlAmdLTwSeDLUMcm0pGWiRVfCEKQlsbGp
+8E8roEmH/Stx/c8ogFPvQIdEnYT3SA9xUdkNew0OOgXlamMKyUN08v94c8zr6zP4
+9quKKDNemOyn7MUmL5bymh+OFw3nhnuQNObRI06Hx05NNImiwcf1swHEktuVT6Bk
+yO1zPAivv2H4gDg+eQyZ5Pl0jrisacoOy0MeCa4Veeff3UZJd33HPdOyrNJTyYzH
+Ub+IcE2LWSS38rOIHUWSOiQKkF2Cf4Y8tyu4fRvOmXE8deG+V7ViIxowbeTBNIY3
+QkOxVVDMjvPqbSjcisozHQp7dYivAu5QWcRqKzMDKI3/eMy1rpzerFQ/1r/oka2Z
+n25nOELfsmmYmdna39QPTFwZxrobdESNuqzqmP+9Igvx42uuG5mbPK5pNXIfjPdk
+OAomfs4zO+kPY27TV0x8A/9lSP7FsNezIGcs7BNsajzQWRQPPNjpvqP/6FocNyr7
+3R83dZziqksHise70/bCMUKkSuX/5f1ohM1KQrCg9+RUHHK6kQKCAQEA+YUg+K6W
++nmrxv26QQp5AYatBaNUiW5oZjIBrU3fweQm7VDBaJZqN3dPMcxQ3HsEfsn3D0hR
+U/hzQ8MV8BrjoC3YoJaBf1dDQr60H0bQNKbCKCWgOGFm5i3bTi8uHqOgfQTQJ4a+
+abauuVtVHMt3z6QQpUIqutf7qn936UIj2Rjv1NoFpmW+K/U6vyyzaxfhH9t1DlPr
+MrLXwPvIcClG1YXdXm6bDLn25UE2kD0u5hyvvUbQYQ16ju4OvkpkLoIHQxFjUUIE
+SsKYf3m/RK667cW7PU74v+0HlgjmVkgTT96FsZ6LqgY5fXVLrka7s+j1CID2w4Dj
+s4R8ghaJ09twmQKCAQEA6BBQ6nUOuqpR+lXXvjsNTZD7MD2aac5nKA3Cx5arpahX
+pqdGphl03wtbArxuwxrhEx68AaRavTlIeZ72y0m+280loRog/JrjU0ZTU3jzZTAZ
+ukXzstAXeLxTG+srMDf7mUF7gvct4onT5cU20O1Ckr295x/NLPq6xStEVgQ2BbrL
+a26ni87anqoun4lnB0WUsCpVjHK/+ivHFM5BFzQmO+iKx8U20Ij3Qoi0jBQ+DhdF
+XjbH3SDE7r/tltwsv8tomCnIWps7vAAyQm88Mh1RXduATPluZfLLmM6tvhSJRJdq
+AxQ5miVmx08l54zxrGI6zJHk/64NkmmTKZ3OXTxlqwKCAQAsto6SAbdMa0E9B3q4
+7QeCHoAi4oHjnsVWit+CDtJqDFhtbms6MroV9mtaoSJcYC8OCWMcefkY8wy0t+DW
+hfsEWTLYlB/gkeKbs1DTyfzFcpyYVSXA9LNbzBvghtPc6bV4scQbUSoOB46H6LX3
+0v5FV0EkXBcMJGgUxYLXaeLCpJVVrzwT9Wd+uRMt7vS33C+bZdg0GRWsoB/JlVT1
+xG/NE4/3vBpMzYZQzr7YWh5tXfagFHCC88dilYZO00Xgj6x9eEAz74CVZQmuzkJY
+LHeS5DwJYH1y5ybU3ANqsr/DMD0E90RP0425zasiL8qzEqvWOkX+ArrLEJK/PQq1
+zD0BAoIBAQC4xGToiBMWJI3o13hTCglpfMnCewn6vE/94Bb5eslnuEUxd3YUwaf/
+/raT0xwNU9Vot8vRMt7cUkOWMi8lZK4Fq60OPBOPjHL61r95co+4PTf+y7tg37YQ
+d0FktTVJywkT2MNSXyO1fy+rff5LEt0yoMgWwYdHDMqwOebK5cdtgHB+NThJZIVE
+VxOQCoJxk8DzEoHStXqM4VY9Botkwiy+/kOhEzC1kJft7ZJzBZry9SxR+yPeuDyU
+K1QsDVnDy1yX6oyPN5Gz+iQKKS6waA9kv2PD5cU0fsAEBmrnMMqqRjQuB2hlhuny
+Pt5bIik5q2xNfMvrltVPgaeeNvsb2P7JAoIBAQDEDJBKM9Ym/BWNLKxCjdzGyegx
+ilWgn0o8r7S5KmS/sdyTtnOFwJTyFeIDBDcsFBogNKWVG7625SMzTYhUcA4DzPPE
+gVJkOfigDoAiN1heN0Ds/I4ysms5syfNQrL8qi80mP/lugrf1fn7DSBOAf4EEbWs
+5BBhJ+I+cZalhbR8hevdRie5C0AVPEuFf92soFd+R/yUhvXrYEjeN8/84GyoACW7
+jO2NBE2EPkSuomRl6Zd7Yce9xsytI9EdMlT+jgGVqNvaTliJBoM2fq6+R8v5iFN7
+zRT9yVmqGJTgjz0E+cV8/0ODbzajfq9JLIj/aICn+BXft7sLt1fJz9fwAwU2
+-----END RSA PRIVATE KEY-----';
+/* DO NOT INDENT THIS CODE end */
+        
+            // Decrypt the data using the private key and store the results in $decrypted
+            openssl_private_decrypt($encrypted, $decrypted, $privkey);
+            
+            $decryptedPassword = $decrypted;
+
+            //Encrypt the password
+            $sUserPassword = $this->oBcrypt->genHash($decryptedPassword);
+        
+            //Update the password for the user
+            $sQuery = $this->conPDO->prepare("UPDATE users SET sUserPassword = :sUserPassword WHERE sUserCreateToken = :sUserToken");
+        
+            $sQuery->bindValue(':sUserPassword', $sUserPassword);
+            $sQuery->bindValue('sUserToken', $aJSONInfo->sUserToken);
+            
+            $sQuery->execute();
+            
+            //Get the sUsername based on the sUserCreateToken
+            $sQuery = $this->conPDO->prepare("SELECT sUsername FROM users WHERE sUserCreateToken = :sUserToken");
+            $sQuery->bindValue(':sUserToken', $aJSONInfo->sUserToken);
+            $sQuery->execute();
+            $result = $sQuery->fetch(PDO::FETCH_ASSOC);
+            $sUsername= $result['sUsername'];
+            
+            //Remove sUserCreateToken
+            $sQuery = $this->conPDO->prepare("UPDATE users SET sUserCreateToken = '' WHERE sUserCreateToken = :sUserToken");
+            $sQuery->bindValue(':sUserToken', $aJSONInfo->sUserToken);
+            $sQuery->execute();            
+            
+            //When data is inserted. log in the user and then redirect user to admin.php, where the user can start creating the menucard
+            if($this->LogInUser($sUsername, $decryptedPassword) == true)
+            {
+                $aUser['result'] = true;
+                return $aUser;
+            }else{
+                 $aUser['result'] = false;
+            }
+        }
+     }
+     
+     public function GetUserinformation()
+     {
+
+         $aUser = array(
+                'sFunction' => 'GetUserinformation',
+                'result' => false
+            );
+         
+         //Check if a session is NOT started
+        if(!isset($_SESSION['sec_session_id']))
+        { 
+            $this->oSecurity->sec_session_start();
+        }
+        
+        //Check if user is logged in
+        if($this->oSecurity->login_check() == true)
+        {
+            $aUser['result'] = true;
+
+            $sUsername = $_SESSION['username'];
+            
+            $aUser['sUsername'] = $sUsername;
+                       
+            //Get iFK_iCompanyId
+            $sQuery = $this->conPDO->prepare("SELECT iFK_iCompanyId FROM users WHERE sUsername = :sUsername LIMIT 1");
+            $sQuery->bindValue(":sUsername", $sUsername);
+            $sQuery->execute();
+            $aResult = $sQuery->fetch(PDO::FETCH_ASSOC);
+            $iFK_iCompanyId = $aResult['iFK_iCompanyId'];
+            
+            //Get company info
+            $sQuery = $this->conPDO->prepare("SELECT * FROM company WHERE iCompanyId = :iFK_iCompanyId");
+            $sQuery->bindValue(":iFK_iCompanyId", $iFK_iCompanyId);
+            $sQuery->execute();
+            $aResult = $sQuery->fetch(PDO::FETCH_ASSOC);
+            
+            $aUser['sCompanyName'] = $aResult['sCompanyName'];
+            $aUser['sCompanyPhone'] = $aResult['sCompanyPhone'];
+            $aUser['sCompanyAddress'] = $aResult['sCompanyAddress'];
+            $aUser['sCompanyZipcode'] = $aResult['sCompanyZipcode'];
+            $aUser['sCompanyCVR'] = $aResult['sCompanyCVR'];
+        }
+        
+        return $aUser;
+         
+     }
+     
+     public function UpdateUserinformation()
+     {
+
+         $aUser = array(
+                'sFunction' => 'UpdateUserinformation',
+                'result' => false
+            );
+         
+         //Check if a session is NOT started
+        if(!isset($_SESSION['sec_session_id']))
+        { 
+            $this->oSecurity->sec_session_start();
+        }
+        
+        //Check if user is logged in
+        if($this->oSecurity->login_check() == true)
+        {
+            
+            if(isset($_GET['sJSON']))
+            {
+                
+                //Get the JSON string
+                $sJSONUserinfo = $_GET['sJSON'];
+                //Convert the JSON string into an array
+                $aJSONUserinfo = json_decode($sJSONUserinfo);
+                
+                //Get iFK_iCompanyId
+                $sQuery = $this->conPDO->prepare("SELECT iFK_iCompanyId FROM users WHERE sUsername = :sUsername LIMIT 1");
+                $sQuery->bindValue(":sUsername", $_SESSION['username']);
+                $sQuery->execute();
+                $aResult = $sQuery->fetch(PDO::FETCH_ASSOC);
+                $iFK_iCompanyId = $aResult['iFK_iCompanyId'];
+
+                //Update the userinfo and the business info
+                $sQuery = $this->conPDO->prepare("UPDATE users SET sUsername = :sUsername WHERE sUsername = :sessionsUsername LIMIT 1");
+                $sQuery->bindValue(":sUsername", $aJSONUserinfo->sUsername);
+                $sQuery->bindValue(":sessionsUsername", $_SESSION['username']);
+                $sQuery->execute();
+
+                //Update company info
+                $sQuery = $this->conPDO->prepare("UPDATE company SET 
+                    sCompanyName = :sCompanyName, sCompanyPhone = :sCompanyPhone, sCompanyAddress = :sCompanyAdress, sCompanyZipcode = :sCompanyZipcode, sCompanyCVR = :sCompanyCVR LIMIT 1");
+                $sQuery->bindValue(":sCompanyName", urldecode($aJSONUserinfo->sCompanyName));
+                $sQuery->bindValue(":sCompanyPhone", urldecode($aJSONUserinfo->sCompanyPhone));
+                $sQuery->bindValue(":sCompanyAdress", urldecode($aJSONUserinfo->sCompanyAddress));
+                $sQuery->bindValue(":sCompanyZipcode", urldecode($aJSONUserinfo->sCompanyZipcode));
+                $sQuery->bindValue(":sCompanyCVR", urldecode($aJSONUserinfo->sCompanyCVR));
+                $sQuery->execute();
+                
+                
+                //Set the new username in the session
+                $_SESSION['username'] = $aJSONUserinfo->sUsername;
+                
+                $aUser['result'] = true;
+            }
+        }
+        
+        return $aUser;
+        
      }
        
 }
