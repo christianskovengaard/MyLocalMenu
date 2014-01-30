260e83a0aac24c957104f14ee9725249605e2cd7
Christian Skovengaard <christian@headnet.dk>
2014-01-30 14:03:26 +0100

    Fixed bug when check for login

--- login.php
+++ login.php
@@ -6,9 +6,11 @@ if(isset($_POST['username']) && isset($_POST['password']))
     require 'Controllers/UserController.php';
     $oUserController = new UserController();
     $loggedIn = $oUserController->LogInUser($_POST['username'], $_POST['password']);
-   if($loggedIn == true){
+   if($loggedIn['result'] == 'true'){
         header('location: admin.php');
-   }else{
+   }else if($loggedIn['result'] == 'Account locked'){
+       header("location: index.php?login=Account_locked");
+   }else if($loggedIn['result'] == 'false'){
        header("location: index.php?login=false");
    }
 }
