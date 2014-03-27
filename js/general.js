9a94c1493e33046aff00d3eb935ea0bc686a74ef
bejtrup <bejtrup@gmail.com>
2014-03-27 21:39:23 +0100

    update EDIT-buttons

--- js/general.js
+++ js/general.js
@@ -25,7 +25,7 @@
       $('.newplaceholder').append('<h3><input type="text" onkeydown="if (event.keyCode == 13) { SaveMenuListHeadlineToHtml(\''+id+'\');}" placeholder="Overskrift"></h3>');
       $('.newplaceholder h3 input').focus();
       $('.newplaceholder').append('<h4><input type="text" onkeydown="if (event.keyCode == 13) { SaveMenuListHeadlineToHtml(\''+id+'\');}" placeholder="evt ekstra info"></h4>');
-      $('.newplaceholder').append('<div class="DishEditWrapper"><div class="EditDish" onclick="EditListHeadline(this)"><img src="img/edit.png"></div><div class="DeleteDish" onclick="DeleteSortableList(this)"><p>╳</p></div></div><input type="hidden" value="new">');
+      $('.newplaceholder').append('<div class="DishEditWrapper"><div class="EditDish" onclick="EditListHeadline(this)"><img src="img/edit_white.png"></div><div class="DeleteDish" onclick="DeleteSortableList(this)"><img src="img/delete_white.png"></div></div><input type="hidden" value="new">');
       $('.DishEditWrapper').hide();
       $('.newplaceholder').append('<ul id="'+id+'" class="connectedSortable"><li></li></ul>');
       $('#'+id).append('<li class="SaveMenuDish"><a class="saveMenuDishButton Cancel" onclick="CancelNewMenuList(this);"> Annuller</a><a class="saveMenuDishButton" onclick="SaveMenuListHeadlineToHtml(\''+id+'\');">✓ Updater</a></li>');
@@ -52,7 +52,7 @@
     $('.DishWrapperTEMP').append('<div class="DishNumber"><input type="text" maxlength="4" placeholder="nr"></div>');
     $('.DishWrapperTEMP').append('<div class="DishText"><div class="DishHeadline"><input type="text" placeholder="Overskrift"></div><div class="DishDescription"><textarea rows="1" placeholder="Beskrivelse"></textarea></div></div>');
     $('.DishWrapperTEMP').append('<div class="DishPrice"><h2>...</h2><input type="text" onkeydown="if (event.keyCode == 13) { SaveMenuDishToHtml();}" placeholder="0"><h2>kr</h2></div>');
-    $('.DishWrapperTEMP').append('<div class="DishEditWrapper DishEditWrapperTEMP"><div class="EditDish" onclick="EditSortableList(this)"><img src="img/edit.png"></div><div class="DeleteDish" onclick="DeleteSortableList(this)"><p>╳</p></div></div>');
+    $('.DishWrapperTEMP').append('<div class="DishEditWrapper DishEditWrapperTEMP"><div class="EditDish" onclick="EditSortableList(this)"><img src="img/edit.png"></div><div class="DeleteDish" onclick="DeleteSortableList(this)"><img src="img/delete.png"></div></div>');
     $('.DishWrapperTEMP').hide().slideDown();
     $('.DishWrapperTEMP').removeClass('DishWrapperTEMP');
 
@@ -129,7 +129,7 @@
       
       if(infoHeadline != ''  && infoDescription != ''){
           
-          $('.InfoSlide.new').before('<div Class="InfoSlide"><h1>'+infoHeadline+'</h1><h2>'+infoDescription+'</h2><div class="DishEditWrapper"><div class="EditDish" onclick="EditInfo(this)"><img src="img/edit.png"></div><div class="DeleteDish" onclick="DeleteSortableList(this)"><p>╳</p></div></div></div>');
+          $('.InfoSlide.new').before('<div Class="InfoSlide"><h1>'+infoHeadline+'</h1><h2>'+infoDescription+'</h2><div class="DishEditWrapper"><div class="EditDish" onclick="EditInfo(this)"><img src="img/edit.png"></div><div class="DeleteDish" onclick="DeleteSortableList(this)"><img src="img/delete.png"></div></div></div>');
           $('.InfoSlide.new').remove();
           $('.SaveMenuDish').fadeOut('normal', function(){ 
               $(this).remove();
