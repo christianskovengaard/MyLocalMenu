b96e8f158d44656d3399ef7333245ab07364743c
christianskovengaard <christianskovengaard@gmail.com>
2014-05-23 11:26:39 +0200

    New importcafe file

--- TESTfiles/importcafe.php
+++ TESTfiles/importcafe.php
@@ -9,63 +9,28 @@ $conPDO =  $oDatabase->ConnectToDatabase();
 
 
 //Read from the csv file
-$file = fopen("cafenavne1.csv","r");
-
-$x = 1;
-
-$aCafe = array(
-  'name' => '',
-  'address' => '',
-  'city' => '',
-  'phonenumber' => '',
-  'webpage' => ''
-  
-);
-
-while(! feof($file)) {
-    //Print array
-    //var_dump(fgetcsv($file));
-    
-    $result = fgetcsv($file);
-    //Insert value into cafe array
-    if($x == 1){
-        $aCafe['name'] = utf8_decode($result[0]);
-        //var_dump($aCafe);
-    }
-    if($x == 2){
-        
-        $aCafe['phonenumber'] = $result[0];
-        //var_dump($aCafe);
-    }
-    if($x == 3){
-        $aCafe['address'] = utf8_decode($result[0]);
-        //var_dump($aCafe);
-    }
-    if($x == 4){
-        
-        $aCafe['city'] = intval($result[0]);
-        //var_dump($aCafe);
-    }
-    if($x == 5){
-        $aCafe['webpage'] = $result[0];
-        //Reset $x
-        $x = 0;      
-        var_dump($aCafe);
-        echo "<br>5. gang Insert to database";
+$file = fopen("Cafénavne_Frederiksberg.csv","r");
+while($result = fgetcsv($file, 'EOF', ";")) {
+    $i++;
+    if($i >= 4) {
+        var_dump($result);
+               
+        //Import format:
+        //[0] = name
+        //[1] = tlf
+        //[2] = address
+        //[3] = zipcode & city
+        //[4] = webpage
         
         //Insert data into database
-        $sQuery = $conPDO->prepare("INSERT INTO restuarentinfo_copy (sRestuarentInfoName,sRestuarentInfoPhone,sRestuarentInfoAddress,iRestuarentInfoZipcode) VALUES (:name,:telephone,:address,:zipcode)");
-        $sQuery->bindValue(":name", $aCafe['name']);
-        $sQuery->bindValue(":telephone", $aCafe['phonenumber']);
-        $sQuery->bindValue(":address", $aCafe['address']);
-        $sQuery->bindValue(":zipcode", $aCafe['city']);
-        $sQuery->execute();
-        
+        $sQuery = $conPDO->prepare("INSERT INTO restuarentname_search (sRestuarentInfoName,sRestuarentInfoPhone,sRestuarentInfoAddress,iRestuarentInfoZipcode) VALUES (:name,:telephone,:address,:zipcode)");
+        $sQuery->bindValue(":name", $result[0]);
+        $sQuery->bindValue(":telephone", $result[1]);
+        $sQuery->bindValue(":address", $result[2]);
+        $sQuery->bindValue(":zipcode", intval($result[3]));
+        //$sQuery->execute();
     }
-    
-    $x++;
 }
-
 fclose($file);
 
 
