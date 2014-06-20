fa7b16385f9499019441845c6c831d13aaa84a25
christianskovengaard <christianskovengaard@gmail.com>
2014-06-20 18:58:13 +0200

    Added ErrorDocument for files not found

---
+++ 404.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, maximum-scale=1.0, minimum-scale=1.0, initial-scale=1.0" />
+        <title>MyLocalCafé</title>
+        <link rel="icon" href="img/favicon.ico"/>
+        <link rel="stylesheet" type="text/css" href="css/jquery-ui-1.8.16.custom.css" />
+        <link rel="stylesheet" type="text/css" href="css/general_index.css" />
+        <link rel="stylesheet" type="text/css" media="only screen and (min-width:50px) and (max-width: 500px)" href="css/general_index_small.css" />
+        <link rel="stylesheet" type="text/css" media="only screen and (min-width:501px) and (max-width: 850px)" href="css/general_index_medium.css" />
+                
+        <!--[if lt IE 9]>
+            <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+        
+        <script type="text/javascript" src="js/jquery-1.7.1.min.js"></script>
+        <script type="text/javascript" src="js/jquery-ui.js"></script>
+        <script type="text/javascript" src="js/general.js"></script>
+
+    </head>
+    <body>
+        
+        <div class="header">
+            <div class="wrapper">
+                <div class="left">
+                  <img src="img/iphone.png">
+                </div>
+                <div class="rigth">
+                  <h1>MyLocal<span>Café</span></h1>
+                  <h3>Vi prøvede alt hvad vi kunne, man vi fandt desværre ikke siden du ledte efter.</h3>
+                  <h3>Har du spørgmål eller brug for hjæp kan du skrive til os på support@mylocalcafe.dk</h3>
+                  <a href="index">Tilbage til forsiden</a>
+                  <br><br>
+                </div>
+            </div>
+        </div>
+        <!--<div class="body">
+            <div class="wrapper">
+                <div class="left">
+                    <h1>Er du cafe ejer?</h1>
+                    <a href="admin">Log ind</a>
+                    <p>eller</p>
+                    <a href="admin">læs mere</a>
+                    <br>
+                    <h3>og få oprettet din café med det samme, helt gratis!</h3>
+                </div>
+                <div class="rigth">
+                    <img class="logo" src="img/logo_4.png">
+                </div>
+            </div>
+        </div>-->
+    </body>
+</html>
